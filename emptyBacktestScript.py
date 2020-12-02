4cd0463badb5b5e5f2aa01e71ffec0ab72c5a609
npp71 <43276994+npp71@users.noreply.github.com>
2020-12-02 14:33:28 -0500

    Add files via upload

---
+++ emptyBacktestScript.py
@@ -0,0 +1,5 @@
+#Create new Order or Add to Previous Order by available account balance
+#Pass in backtrader timeframe into "Strategy"
+#Refresh "Strategy" every day at open
+#Test current order against best interval : decide to change Interval or not. (Trailing Stop?)
+#Update Order
